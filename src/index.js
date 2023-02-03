b24128948b4671fc451d0dee034ff6408bff6e58
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2023-02-03 14:19:07 -0500

    initial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
 import allTransactions from './all_transactions.js';
 import botwData from './botw_data.js';
-import botwEquipment from './botw_equipment_cleaned.js';
-import botwMonsters from './botw_monsters_cleaned.js';
 
+const categories = [];
+Object.keys(botwData.data).forEach(element => {
+        categories.push(element);
+});
 
-const equipment = botwEquipment;
-const monsters = botwMonsters;
-const materials = botwData.data.materials;
+const data = []
+
+let categoriesLength = 0;
+
+categories.forEach((category)=>{
+    categoriesLength+=1;
+})
+
+for(let i = 0; i<categoriesLength; i++){
+    data[i] = botwData.data[categories[i]];
+}
+
+let index;
+for(let i = 0; i<categoriesLength; i++){
+    index = i;
+
+    if(categories[index] !== "creatures" &&
+    categories[index] !== "equipment" &&
+    categories[index] !== "treasure" &&
+    categories[index] !== "materials" &&
+    categories[index] === "monsters"){
+        i=categoriesLength;
+    }
+}
+
+const monsterData = data[index];
+const neededData = {};
+
+monsterData.forEach((monster)=>{
+neededData[monster.name] = monster.drops;
+})
+
+const keys = Object.keys(neededData);
+const moreNeededData = {}
+for(let i = 0;i<keys.length; i++){
+    if(neededData[keys[i]] !== null && neededData[keys[i]].includes("ruby")){
+        moreNeededData[keys[i]] = neededData[keys[i]];
+    }
+}
+
+const moreKeys = Object.keys(moreNeededData);
+const final = [];
+for(let i = 0;i<moreKeys.length; i++){
+    final[i] = moreKeys[i] + " : " + moreNeededData[moreKeys[i]]; 
+}
+
+debugger;
