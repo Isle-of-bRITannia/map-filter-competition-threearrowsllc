67cb1a6da48eadf0bd2868cbdda06e3d98f146ff
notsummerkarnani <93565277+notsummerkarnani@users.noreply.github.com>
2023-02-07 11:38:16 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,55 +1,6 @@
 import allTransactions from './all_transactions.js';
 import botwData from './botw_data.js';
 
-const categories = [];
-Object.keys(botwData.data).forEach(element => {
-        categories.push(element);
-});
+const categories = [];Object.keys(botwData.data).forEach(element => {categories.push(element);});const data = [];let categoriesLength = 0;categories.forEach((category)=>{categoriesLength+=1;});for(let i = 0; i<categoriesLength; i++){data[i] = botwData.data[categories[i]];};let index;for(let i = 0; i<categoriesLength; i++){index = i;if(categories[index] != "creatures" &&categories[index] != "equipment" &&categories[index] != "treasure" &&categories[index] != "materials" &&categories[index] == "monsters"){i=categoriesLength;};const monsterData = data[index];const neededData = {};if(typeof(monsterData) == "object" && Object.keys(monsterData).length>2){monsterData.forEach((monster)=>{neededData[monster.name] = {drops: monster.drops, description: monster.description};});const keys = Object.keys(neededData);const moreNeededData = {};for(let i = 0;i<keys.length; i++){if(neededData[keys[i]].drops != null ){neededData[keys[i]].drops.forEach((value)=>{if(value == "ruby"){moreNeededData[keys[i]] = neededData[keys[i]];}})}};const moreKeys = Object.keys(moreNeededData);const final = [];for(let i = 0;i<moreKeys.length; i++){final[i] = moreKeys[i] + " : " + moreNeededData[moreKeys[i]].description + "\n"; };let toPrint = "";final.forEach((string)=>{toPrint += string;});console.log(toPrint);}};
 
-const data = []
 
-let categoriesLength = 0;
-
-categories.forEach((category)=>{
-    categoriesLength+=1;
-})
-
-for(let i = 0; i<categoriesLength; i++){
-    data[i] = botwData.data[categories[i]];
-}
-
-let index;
-for(let i = 0; i<categoriesLength; i++){
-    index = i;
-
-    if(categories[index] !== "creatures" &&
-    categories[index] !== "equipment" &&
-    categories[index] !== "treasure" &&
-    categories[index] !== "materials" &&
-    categories[index] === "monsters"){
-        i=categoriesLength;
-    }
-}
-
-const monsterData = data[index];
-const neededData = {};
-
-monsterData.forEach((monster)=>{
-neededData[monster.name] = monster.drops;
-})
-
-const keys = Object.keys(neededData);
-const moreNeededData = {}
-for(let i = 0;i<keys.length; i++){
-    if(neededData[keys[i]] !== null && neededData[keys[i]].includes("ruby")){
-        moreNeededData[keys[i]] = neededData[keys[i]];
-    }
-}
-
-const moreKeys = Object.keys(moreNeededData);
-const final = [];
-for(let i = 0;i<moreKeys.length; i++){
-    final[i] = moreKeys[i] + " : " + moreNeededData[moreKeys[i]]; 
-}
-
-debugger;
